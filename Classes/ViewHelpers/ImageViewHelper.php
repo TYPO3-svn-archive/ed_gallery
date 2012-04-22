e10dfe4b7f2cf4a4708c5bbaba88038766b12bfd
edots <edots@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-22 19:43:48 +0000

    removed viewhelpers which are now moved (and improved) to depending extbase_hijax extension. updated default template sample in order to demonstrate how the new viewhelpers should be used. pagination is now fully ajaxified with non-JS fallback.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ed_gallery/trunk@60995 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/ViewHelpers/ImageViewHelper.php
+++ Classes/ViewHelpers/ImageViewHelper.php
@@ -1,47 +1,27 @@
 <?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Nikola Stojiljkovic <nikola.stojiljkovic(at)essentialdots.com>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
-/*                                                                        *
- * This script is part of the TYPO3 project - inspiring people to share!  *
- *                                                                        *
- * TYPO3 is free software; you can redistribute it and/or modify it under *
- * the terms of the GNU General Public License version 2 as published by  *
- * the Free Software Foundation.                                          *
- *                                                                        *
- * This script is distributed in the hope that it will be useful, but     *
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
- * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General      *
- * Public License for more details.                                       *
- *                                                                        */
-
-/**
- * Resizes a given image (if required) and renders the respective img tag
- *
- * = Examples =
- *
- * <code title="Default">
- * <f:image src="EXT:myext/Resources/Public/typo3_logo.png" alt="alt text" />
- * </code>
- * <output>
- * <img alt="alt text" src="typo3conf/ext/myext/Resources/Public/typo3_logo.png" width="396" height="375" />
- * or (in BE mode):
- * <img alt="alt text" src="../typo3conf/ext/viewhelpertest/Resources/Public/typo3_logo.png" width="396" height="375" />
- * </output>
- *
- * <code title="Inline notation">
- * {f:image(src: 'EXT:viewhelpertest/Resources/Public/typo3_logo.png', alt: 'alt text', minWidth: 30, maxWidth: 40)}
- * </code>
- * <output>
- * <img alt="alt text" src="../typo3temp/pics/f13d79a526.png" width="40" height="38" />
- * (depending on your TYPO3s encryption key)
- * </output>
- *
- * <code title="non existing image">
- * <f:image src="NonExistingImage.png" alt="foo" />
- * </code>
- * <output>
- * Could not get image resource for "NonExistingImage.png".
- * </output>
- */
 class Tx_EdGallery_ViewHelpers_ImageViewHelper extends Tx_Fluid_ViewHelpers_ImageViewHelper {
 
 	/**
@@ -58,8 +38,6 @@ class Tx_EdGallery_ViewHelpers_ImageViewHelper extends Tx_Fluid_ViewHelpers_Imag
 	 * @param string $ext
 	 *
 	 * @return string rendered tag.
-	 * @author Sebastian Böttger <sboettger@cross-content.com>
-	 * @author Bastian Waidelich <bastian@typo3.org>
 	 */
 	public function render($src, $width = NULL, $height = NULL, $minWidth = NULL, $minHeight = NULL, $maxWidth = NULL, $maxHeight = NULL, $ext = NULL) {
 		if (TYPO3_MODE === 'BE') {
